2fbf0bb0b2f8d89d00e2e09ac9c2b361650cff40
Leonex6577 <ale422515@gmail.com>
2025-05-12 16:29:11 -0600

    Update tapiahernandezjosealejandro.py

--- CLass/tapiahernandezjosealejandro.py
+++ CLass/tapiahernandezjosealejandro.py
@@ -1,3 +1,4 @@
+#Tapia Hernandez Jose Alejandro 
 class Computadoras:
     def __init__ (self, cpu, ram,  grafica, placa_madre):
         self.cpu = cpu
@@ -8,3 +9,5 @@ class Computadoras:
 gaimer = Computadoras("AMD Ryzen 5 3600X", "ddr4", "Nvidia1660", "Asus")
 
 print(gaimer.cpu)
+
+#El funcionamiento del codigo es el siguiente primero creamos una clase en este ejemplo computadoras y poniendo la definicion de los componentes luego con self para guadarlos y al final  aplicamos eso en una clase con un nombre y los componentes con nombre ´´para al final imprimir
